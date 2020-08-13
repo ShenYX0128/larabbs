301f0ce75c18e663247a54c5e7c369974ef9a318
ShenYX0128 <903422096@qq.com>
2020-08-13 10:23:33 +0000

    裁剪头像

--- app/Handlers/ImageUploadHandler.php
+++ app/Handlers/ImageUploadHandler.php
@@ -2,12 +2,14 @@
 
 namespace App\Handlers;
 
+use Image;
+
 class ImageUploadHandler
 {
     //只允许以下后缀名的图片文件上传
     protected $allowed_ext = ['png','jpg','gif','jpeg'];
 
-    public function save($file,$folder,$file_prefix)
+    public function save($file,$folder,$file_prefix,$max_width = false)
     {
         // 文件后缀名
         $extension = strtolower($file->getClientOriginalExtension()) ?: 'png';
@@ -20,10 +22,29 @@ class ImageUploadHandler
         $filename = $file_prefix. '_' . time() . '_' .str_random(10) . '.' . $extension;
 
         $file->move($upload_path,$filename);
-
+        if($max_width && $extension != 'gif') {
+            $this->reduceSize($upload_path . '/' . $filename,$max_width);
+        }
         return [
             'path' => config('api.url')."/$folder_name/$filename"
         ];
 
+    }
+
+    public function reduceSize($file_path, $max_width)
+    {
+        // 实例化
+        $image = Image::make($file_path);
+
+
+        $image->resize($max_width,null,function ($constraint) {
+            //设定宽度是$max_width,高度等比例双方缩放
+            $constraint->aspectRatio();
+
+            //防止截图时图片尺寸变大
+            $constraint->upsize();
+        });
+        //对图片修改后进行保存
+        $image->save();
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
       <div class="card">
-        <img src="{{ $user->avatar }}" alt="{{ $user->name }}" width="255px" height="255px">
+        <img src="{{ $user->avatar }}" alt="{{ $user->name }}">
         <div class="card-body">
           <h5><strong>个人简历</strong></h5>
           <p>{{ $user->introduction }}</p>
