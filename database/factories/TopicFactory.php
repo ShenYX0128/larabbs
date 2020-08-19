6bf1b459eab12af4fd6a396e39e470b5dcb0609b
ShenYX0128 <903422096@qq.com>
2020-08-19 09:35:36 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,16 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    $updated_at = $faker->dateTimeThisMonth();//随机去一个月以内的时间
+    $created_at = $faker->dateTimeThisMonth($updated_at);//生成最大不超过参数的时间
+
     return [
         // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
