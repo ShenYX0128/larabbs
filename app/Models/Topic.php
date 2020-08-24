94ffe6b766ab53719de6b861978cbe5a1e1a6499
ShenYX0128 <903422096@qq.com>
2020-08-24 07:26:13 +0000

    用户可以创建话题

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'ecxept', 'slug'];
+    protected $fillable = ['title', 'body', 'category_id', 'ecxept', 'slug'];
 
     public function category()
     {
